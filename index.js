668004c8482ac3a9385a7dad8fede05dd989bd1b
Chethan shetty <chethanv997@gmail.com>
2026-01-03 16:04:14 +0530

    React episode 3

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ import ReactDOM from "react-dom/client";
 
 //React element
 const heading = React.createElement("h1", {}, "Chethan shetty");
-const jsxheading = <h1>Namaste react using jsx</h1>;
+//babel transiples this jsx code in intp React.createElement
+const jsxheading = <h1 className="headingjsx">Namaste react using jsx</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxheading);
-
-
 //JSX heading
-
